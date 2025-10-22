92350af61437264a4d300947251aa42c3c5314ae
Naosousenior <132512670+Naosousenior@users.noreply.github.com>
2025-10-22 11:09:26 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:parry_front/ui/screen_login.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    home: Login(),
+    routes: <String, WidgetBuilder> {
+      '/sla_porra': (BuildContext c) => Scaffold(
+        appBar: AppBar(
+            
+        ),
+      )
+    }
+  )
+  );
+}
---
+++ lib/ui/screen_login.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget{
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext contexto) {
+    return Row(
+      spacing: 5,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Center(
+          child: Image.asset('assets/imagens/logo.png'),
+        ),
+        SizedBox(
+          height: 500,
+          width: 400,
+          child: Card(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                spacing: 10,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'e-mail'
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'senha'
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: Text('Entrar'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
