8e95229ccdbb7db39e6e48b80e2b2da05a24c122
Naosousenior <132512670+Naosousenior@users.noreply.github.com>
2025-10-28 11:30:28 -0300

    teste
    aaaa

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:parry_front/ui/screen_login.dart';
 
 void main(List<String> args) {
+  print("aaaaaa");
   runApp(MaterialApp(
     theme: ThemeData.dark(),
     home: Login(),
